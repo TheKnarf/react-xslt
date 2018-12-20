d0db6833e4de0b26942d06702ae2e36870755a54
Frank Lyder Bredland <fl@nyggen.com>
2018-12-20 10:08:57 +0100

    Started setting up testing

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
-const React = require('react');
-const xpath = require('xpath.js');
-const dom = require('xmldom').DOMParser;
+const React = require('react'),
+		xpath = require('xpath.js'),
+		DOMParser = require('xmldom'),
+		dom = React.createElement;
 
 const ApplyTemplate = ({ xml, select, Component }) => 
 	(xml !== null && typeof xml !== 'undefined') ?
 		((xpath(xml, select)||[])
 			.map(xml => <Component xml={xml} />)
-		|| <div />) : <div />;
+		|| []) : [];
 
 const ValueOf = ({ xml, select }) => {
 	if(xml == null || typeof xml == 'undefined')
 		return <span />;
 	
 	const query = xpath(xml, select)[0],
-			output = query.nodeValue || query.firstChild.data || <span />;
+			output = query.nodeValue || query.firstChild.data || [];
 
 	return output;
 }
 
-module.exports = { xpath, dom, ApplyTemplate, ValueOf };
+module.exports = { xpath, dom: DOMParser, ApplyTemplate, ValueOf };
---
+++ test/test.js
@@ -0,0 +1,40 @@
+import assert from 'assert';
+import { dom as iso } from 'isomorphic-jsx';
+import React from 'React';
+import { xpath, dom as XMLDom , ValueOf, ApplyTemplate } from 'react-xslt';
+
+describe('react-xslt', () => {
+
+	it('simple test', () => {
+		var dom = iso;
+		const xmlString = <root>
+			<test name="test1" />
+			<test name="test2" />
+			<test name="test3" />
+			<test name="test4" />
+		</root>;
+
+		dom = React.createElement;
+		const inquiry = ({xml}) =>
+			<div className="inquiry">
+				<ValueOf xml={xml} select="." />
+			</div>;
+
+		const Section = ({xml}) => {
+			return <section>
+				<h1>
+					<ValueOf xml={xml} select="@title" />
+				</h1>
+				<p> <ValueOf xml={xml} select="description" /> </p>
+				<h2> Kontrollpunk </h2>
+				<ApplyTemplate xml={xml} select="inquiry" Component={inquiry} />
+			</section>;
+		}
+
+		// TODO: simplify examples over to 1 or 2 simple React components
+		// TODO: setup JSDOM for testing: https://github.com/airbnb/enzyme/blob/master/docs/guides/jsdom.md
+
+		assert.equal(true, true);
+	})
+
+})
